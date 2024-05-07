269c7858cc2e860f3a13252d44457cb7652532b5
Renee Gula <81697387+rajgg0218@users.noreply.github.com>
2024-05-07 14:20:06 +0800

    Update Revising-the-Select-Query-I.sql
    first commit

--- Revising-the-Select-Query-I.sql
+++ Revising-the-Select-Query-I.sql
@@ -4,15 +4,15 @@ EVISING THE SELECT QUERY I
 Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA. The CITY table is described as follows:
 
 CITY
----------------------------------
-Field		   | Type
----------------------------------
-ID		      | NUMBER 
-NAME 		   | VARCHAR2 (17)
-COUNTRYCODE	| VARCHAR (3)
-DISTRICT	   | VARCHAR2 (20)
-POPULATION	| NUMBER 
----------------------------------
+-----------------------------------------------
+Field		                | Type
+-----------------------------------------------
+ID		                   | NUMBER 
+NAME 		                | VARCHAR2 (17)
+COUNTRYCODE	             | VARCHAR (3)
+DISTRICT	                | VARCHAR2 (20)
+POPULATION	             | NUMBER 
+------------------------------------------------
 */
 SELECT
     *
