31218a64c5c5f365673700961d4bf5590a110e00
Renee Gula <81697387+rajgg0218@users.noreply.github.com>
2024-05-07 14:21:04 +0800

    Update Revising-the-Select-Query-I.sql
    first commit

--- Revising-the-Select-Query-I.sql
+++ Revising-the-Select-Query-I.sql
@@ -5,13 +5,13 @@ Query all columns for all American cities in the CITY table with populations lar
 
 CITY
 -----------------------------------------------
-Field		                | Type
+FIELD                   |   TYPE
 -----------------------------------------------
-ID		                   | NUMBER 
-NAME 		                | VARCHAR2 (17)
-COUNTRYCODE	             | VARCHAR (3)
-DISTRICT	                | VARCHAR2 (20)
-POPULATION	             | NUMBER 
+ID		                  |   NUMBER 
+NAME 		               |   VARCHAR2 (17)
+COUNTRYCODE	            |   VARCHAR (3)
+DISTRICT	               |   VARCHAR2 (20)
+POPULATION	            |   NUMBER 
 ------------------------------------------------
 */
 SELECT
