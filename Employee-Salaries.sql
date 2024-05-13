daaa1aa2783ebe94f1b74c4cb4148ee59a2d4920
Renee Gula <81697387+rajgg0218@users.noreply.github.com>
2024-05-14 00:22:28 +0800

    Create Employee-Salaries.sql

---
+++ Employee-Salaries.sql
@@ -0,0 +1,22 @@
+/*
+Write a query that prints a list of employee names (i.e.: the name attribute) 
+for employees in employees having a salary greater than 2000 dollars per month who have been employees for less than 10 months. 
+Sort your result by ascending employee_id.
+
+Input Format
+The Employee table containing employee data for a company is described as follows:
+------------------------------------
+COLUMN           |   TYPE
+------------------------------------
+ID               |    Integer
+NAME             |    String
+MONTHS           |    Integer
+SALARY           |    Integer
+------------------------------------
+where employee_id is an employee's ID number, name is their name,
+months is the total number of months they've been working for the company, 
+and salary is their monthly salary.
+
+Sample Input
+
+*/
