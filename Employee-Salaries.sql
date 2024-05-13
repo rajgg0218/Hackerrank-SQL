dd5455011bdc9b58d12b2f2c6a76efa348c0e749
Renee Gula <81697387+rajgg0218@users.noreply.github.com>
2024-05-14 00:28:11 +0800

    Update Employee-Salaries.sql

--- Employee-Salaries.sql
+++ Employee-Salaries.sql
@@ -18,5 +18,34 @@ months is the total number of months they've been working for the company,
 and salary is their monthly salary.
 
 Sample Input
+------------------------------------------------------------------------------
+EMPLOYEE_ID      |    NAME          |    MONTHS          |    SALARY
+------------------------------------------------------------------------------
+12228            |    Rose          |    15              |    1968 
+33645            |    Angela        |    1               |    3443 
+45692            |    Frank         |    17              |    1608
+56118            |    Patrick       |    7               |    1345
+59725            |    Lisa          |    11              |    2330
+47195            |    Kimberly      |    16              |    4372
+78454            |    Bonnie        |    8               |    1771
+83565            |    Michael       |    6               |    2017
+98607            |    Todd          |    5               |    3396
+99989            |    Joe           |    9               |    3573
+------------------------------------------------------------------------------
+Sample Output
+Angela
+Michael
+Todd
+Joe
 
+Explanation
+Angela has been an employee for 1 month and earns $3443 per month.
+Michael has been an employee for 6 months and earns $2017 per month.
+Todd has been an employee for 5 months and earns $3396 per month.
+Joe has been an employee for 9 months and earns $3573 per month.
+We order our output by ascending employee_id.
 */
+SELECT name
+FROM Employee
+WHERE salary > 2000 AND months < 10
+ORDER BY employee_id ASC;
