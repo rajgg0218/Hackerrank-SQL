bb063558a106972def9664499f249822cadf48bc
Renee Gula <81697387+rajgg0218@users.noreply.github.com>
2024-05-15 00:20:21 +0800

    Update Revising-Aggregations-The-Count-Function.sql

--- Revising-Aggregations-The-Count-Function.sql
+++ Revising-Aggregations-The-Count-Function.sql
@@ -2,5 +2,20 @@
 Query a count of the number of cities in CITY having a Population larger than 100,000.
 
 Input Format
+The CITY table is described as follows:
+------------------------------------------------
+FIELD                |   TYPE
+------------------------------------------------
+ID                   |  NUMBER 
+NAME                 |  VARCHAR2 (17)
+COUNTRYCODE          |  VARCHAR2 (3)
+DISTRICT             |  VARCHAR2 (20)
+POPULATION           |  NUMBER  
+-------------------------------------------------
 
+SOLUTION
 */
+SELECT COUNT(*)
+FROM CITY
+WHERE Population > 100000;
+
