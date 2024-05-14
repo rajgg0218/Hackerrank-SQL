f06c8ab268df6ac39e9c5c36de7330d2140c9e02
Renee Gula <81697387+rajgg0218@users.noreply.github.com>
2024-05-15 00:44:22 +0800

    Create Average-Population.sql

---
+++ Average-Population.sql
@@ -0,0 +1,9 @@
+/*
+Query the average population for all cities in CITY, rounded down to the nearest integer.
+
+Input Format
+
+SOLUTION
+*/
+SELECT FLOOR(AVG(Population)) AS Average_Population
+FROM CITY;
