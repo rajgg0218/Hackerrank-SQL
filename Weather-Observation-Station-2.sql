3a04ec26ca3a00febb753731702db053fe02c4e7
Renee Gula <81697387+rajgg0218@users.noreply.github.com>
2024-05-17 22:50:15 +0800

    Update Weather-Observation-Station-2.sql

--- Weather-Observation-Station-2.sql
+++ Weather-Observation-Station-2.sql
@@ -7,7 +7,15 @@ Query the following two values from the STATION table:
 Input Format
 
 The STATION table is described as follows:
-
+------------------------------------------------
+FIELD                |   TYPE
+------------------------------------------------
+ID                   |  NUMBER 
+CITY                 |  VARCHAR2 (21)
+STATE                |  VARCHAR2 (2)
+LAT_N                |  NUMBER
+LONG_W               |  NUMBER  
+-------------------------------------------------
 where LAT_N is the northern latitude and LONG_W is the western longitude.
 
 Output Format
