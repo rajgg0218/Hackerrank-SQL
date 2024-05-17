676517f46c28240650bee8bd101bbd46458b2139
Renee Gula <81697387+rajgg0218@users.noreply.github.com>
2024-05-17 22:37:13 +0800

    Update Population-Density-Difference.sql

--- Population-Density-Difference.sql
+++ Population-Density-Difference.sql
@@ -1,6 +1,16 @@
 /*
 Query the difference between the maximum and minimum populations in CITY.
 
+The CITY table is described as follows:
+------------------------------------------------
+FIELD                |   TYPE
+------------------------------------------------
+ID                   |  NUMBER 
+NAME                 |  VARCHAR2 (17)
+COUNTRYCODE          |  VARCHAR2 (3)
+DISTRICT             |  VARCHAR2 (20)
+POPULATION           |  NUMBER  
+-------------------------------------------------
 
 SOLUTION
 */
