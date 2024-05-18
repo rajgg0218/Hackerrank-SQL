273c8c7b08139f05732ae8db47ad5abb97f88d33
Renee Gula <81697387+rajgg0218@users.noreply.github.com>
2024-05-18 22:46:57 +0800

    Update Weather-Observation-Station-14.sql

--- Weather-Observation-Station-14.sql
+++ Weather-Observation-Station-14.sql
@@ -1,5 +1,19 @@
 /*
-Query the greatest value of the Northern Latitudes (LAT_N) from STATION that is less than 137.2345. Truncate your answer to 4 decimal places.
+Query the greatest value of the Northern Latitudes (LAT_N) from STATION that is less than 137.2345. 
+Truncate your answer to 4 decimal places.
+
+Input Format
+The STATION table is described as follows:
+------------------------------------------------
+FIELD                |   TYPE
+------------------------------------------------
+ID                   |  NUMBER 
+CITY                 |  VARCHAR2 (21)
+STATE                |  VARCHAR2 (2)
+LAT_N                |  NUMBER
+LONG_W               |  NUMBER  
+-------------------------------------------------
+where LAT_N is the northern latitude and LONG_W is the western longitude.
 */
 SELECT TRUNCATE (MAX(LAT_N),4)
 FROM STATION
