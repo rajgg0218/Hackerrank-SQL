114891ccc2b2a402f4497fc5f37acb3c274ac545
Renee Gula <81697387+rajgg0218@users.noreply.github.com>
2024-05-20 23:36:16 +0800

    Update Weather-Observation-Station-18.sql

--- Weather-Observation-Station-18.sql
+++ Weather-Observation-Station-18.sql
@@ -1,5 +1,5 @@
 /*
-Consider P1(a,b) and P2(c,d)  to be two points on a 2D plane.
+Consider P1(a,b) and P2(c,d) to be two points on a 2D plane.
 
 a happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
 b happens to equal the minimum value in Western Longitude (LONG_W in STATION).
