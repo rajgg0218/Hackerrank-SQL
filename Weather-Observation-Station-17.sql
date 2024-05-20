acfdd00fe303818b344b02597c3290be74a80db0
Renee Gula <81697387+rajgg0218@users.noreply.github.com>
2024-05-20 23:30:17 +0800

    Update Weather-Observation-Station-17.sql

--- Weather-Observation-Station-17.sql
+++ Weather-Observation-Station-17.sql
@@ -2,6 +2,17 @@
 Query the Western Longitude (LONG_W)where the smallest Northern Latitude (LAT_N) in STATION  is greater than 38.7780. 
 Round your answer to 4 decimal places.
 
+Input Format
+The STATION table is described as follows:
+FIELD                |   TYPE
+------------------------------------------------
+ID                   |  NUMBER 
+CITY                 |  VARCHAR2 (21)
+STATE                |  VARCHAR2 (2)
+LAT_N                |  NUMBER
+LONG_W               |  NUMBER  
+-------------------------------------------------
+where LAT_N is the northern latitude and LONG_W is the western longitude.
 */
 SELECT ROUND(LONG_W, 4) AS longitude
 FROM STATION
