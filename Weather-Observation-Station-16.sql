06c4b075925839b14ad989e52c1ede833299cc88
Renee Gula <81697387+rajgg0218@users.noreply.github.com>
2024-05-20 23:17:15 +0800

    Update Weather-Observation-Station-16.sql

--- Weather-Observation-Station-16.sql
+++ Weather-Observation-Station-16.sql
@@ -4,6 +4,15 @@ Round your answer to 4 decimal places.
 
 Input Format
 The STATION table is described as follows:
+FIELD                |   TYPE
+------------------------------------------------
+ID                   |  NUMBER 
+CITY                 |  VARCHAR2 (21)
+STATE                |  VARCHAR2 (2)
+LAT_N                |  NUMBER
+LONG_W               |  NUMBER  
+-------------------------------------------------
+where LAT_N is the northern latitude and LONG_W is the western longitude.
 */
 
 SELECT ROUND(MIN(LAT_N), 4) AS smallest_latitude
