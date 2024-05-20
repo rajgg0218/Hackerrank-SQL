e15e9f042ea15692d722183de0fa3cbb268cd542
Renee Gula <81697387+rajgg0218@users.noreply.github.com>
2024-05-20 23:36:04 +0800

    Update Weather-Observation-Station-18.sql

--- Weather-Observation-Station-18.sql
+++ Weather-Observation-Station-18.sql
@@ -6,6 +6,18 @@ b happens to equal the minimum value in Western Longitude (LONG_W in STATION).
 c happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
 d happens to equal the maximum value in Western Longitude (LONG_W in STATION).
 Query the Manhattan Distance between points P1 and P2 and round it to a scale of 4 decimal places.
+
+Input Format
+The STATION table is described as follows:
+FIELD                |   TYPE
+------------------------------------------------
+ID                   |  NUMBER 
+CITY                 |  VARCHAR2 (21)
+STATE                |  VARCHAR2 (2)
+LAT_N                |  NUMBER
+LONG_W               |  NUMBER  
+-------------------------------------------------
+where LAT_N is the northern latitude and LONG_W is the western longitude.
 */
 SELECT ROUND(
     ABS(MIN(LAT_N) - MAX(LAT_N)) + ABS(MIN(LONG_W) - MAX(LONG_W)),4) 
